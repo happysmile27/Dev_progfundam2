0081b79de209a6a186882a062d2c5e6a72b192d4
Olia <61459741+happysmile27@users.noreply.github.com>
2020-06-03 22:33:47 +0300

    Update Task8.py

--- Task8.py
+++ Task8.py
@@ -1,22 +1,9 @@
 my_dict = {'a': 100, 'b': 200, 'c': 300, 'd': 10, 'e': 1000, 'f': 250}
 def my_func(dictionary, n):
-    sorted(dictionary, key=dictionary.get, reverse=True)
-
-    # for x in dictionary.keys():
-    #     if x == n:
-    #         break
-    #     else:
-    #         x += 1
-
-
-
-
-
-    # for key in sorted(dictionary.keys(), reverse=True):
-    #     item = dictionary.pop(key)
-    # for i in range(n):
-    #     item = dictionary.popitem()
-    # print(item)
+    if n < len(dictionary.keys()) / 2:
+        d = sorted(dictionary, key=dictionary.get, reverse=True)
+        print(d[:n])
+    else:
+        print("Error")
 
 print(my_func(my_dict, 2))
-
