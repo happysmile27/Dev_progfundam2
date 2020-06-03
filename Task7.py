b9c2dd6d11648cc4ee7ddcee32d90fb8a935f163
Olia <61459741+happysmile27@users.noreply.github.com>
2020-06-03 22:34:27 +0300

    Update Task7.py

--- Task7.py
+++ Task7.py
@@ -1,23 +1,24 @@
 my_list = [1, 2, 4, 0, 4, 0, 10, 20, 0, 1]
-# new_list = []
-#
-# try:
-#     new_list = list(map(lambda x: 2 / x, my_list))
-# except ZeroDivisionError:
-#     pass
-#
-# print(new_list)
+new_list = list(filter(lambda x: x != 0, my_list))
+
+try:
+    new = list(map(lambda x: 2 / x, new_list))
+except ZeroDivisionError:
+    pass
+
+print(new)
 
 
 
-def devis(n, list):
-    new_list = []
-    for i, m_list in enumerate(list):
-        try:
-            new_list.append(n/m_list)
-        except ZeroDivisionError:
-            new_list.append(None)
-    return new_list
-print(devis(2, my_list))
 
+
+# def devis(n, list):
+#     new_list = []
+#     for i, m_list in enumerate(list):
+#         try:
+#             new_list.append(n/m_list)
+#         except ZeroDivisionError:
+#             new_list.append(None)
+#     return new_list
+# print(devis(2, my_list))
 
