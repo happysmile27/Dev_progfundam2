a69720236d029f9dd6862a3d672958f30276ce78
Olia <61459741+happysmile27@users.noreply.github.com>
2020-06-03 22:35:13 +0300

    Update Task11.py

--- Task11.py
+++ Task11.py
@@ -11,7 +11,3 @@ def create_identity_matrix(n):
 #
 #     return mat
 #
-# def main():
-#     alpha = ['a','b','c','d','e','f','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-#     mat = createMatrix(5,5,alpha)
-#     print (mat)
