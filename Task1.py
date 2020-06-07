1f316e9ff99bc2d961e38139183ca493fe9da2c0
Olia <61459741+happysmile27@users.noreply.github.com>
2020-06-07 18:42:01 +0300

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,9 @@
 num_list = [5, 2, 3, 4, 0]
+def get_variance(num):
+    n = len(num)
+    temp = 0
+    for i in num:
+        temp += (i - (sum(num) / n)) ** 2
+    result = temp / n
+    print(result)
+get_variance(num_list)
