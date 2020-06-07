5398ccbda7b4f554dbb3f5c36c2da90c944858c9
Olia <61459741+happysmile27@users.noreply.github.com>
2020-06-07 18:56:13 +0300

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,7 +1,5 @@
 dirty_string = ['blue1', 'green1', 'pink1', 'yellow1', 'red1']
-# upper = dirty_string[x][0].upper() #x[0] = upper
-# remove_num = dirty_string[x][-1].replace(str(1), "") #x[-1] = remove
-# rules = upper + remove_num
-# print(list(map(lambda x: rules, dirty_string)))
-# # print(list(map(lambda x: x[0] = upper, remove_num, dirty_string)))
+
+a = list(map(lambda x: x[:1:].capitalize() + x[1:-1:], dirty_string))
+print(a)
 
