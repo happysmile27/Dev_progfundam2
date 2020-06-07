1215170c42c2b111ceec83f385fbd02b34a1bde0
Olia <61459741+happysmile27@users.noreply.github.com>
2020-06-07 18:38:26 +0300

    Update Task11.py

--- Task11.py
+++ Task11.py
@@ -1,4 +1,18 @@
-import numpy as np
+# import numpy as np
 
-identity_matrix = np.identity(7)
-print(identity_matrix)
+# identity_matrix = np.identity(7)
+# print(identity_matrix)
+
+
+def create_identity_matrix(n):
+    id_matrix = []
+    for i in range(n):
+        row_list = []
+        for j in range(n):
+            if i == j:
+                row_list.append(1)
+            else:
+                row_list.append(0)
+            id_matrix.append(row_list)
+        print(row_list)
+create_identity_matrix(7)
